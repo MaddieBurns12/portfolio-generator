97ee327b986c16b410a91ed66db48bcd1ca287fc
MaddieBurns12 <mburns6023@gmail.com>
2022-08-04 17:18:20 -0400

    working on potion function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -144,8 +144,15 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
-    });
+            .then(promptProject)
+            .then(portfolioData => {
+                const pageHTML = generatePage(portfolioData);
+
+                // fs.writeFile('./index.html', pageHTML, err => {
+                //   if (err) throw new Error(err);
+
+                //   console.log('Page created! Check out index.html in this directory to see it!');
+                // });
+  });
+
     
